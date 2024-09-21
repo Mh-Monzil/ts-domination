8850639d9f996db409735b627d70c7ef028f629e
Mh-Monzil <monzil246@gmail.com>
2024-09-22 00:37:16 +0600

    static

---
+++ static/app.ts
@@ -0,0 +1,9 @@
+// don't need to create instance (new) to access data from class
+
+class python {
+    static version = 1.0;
+
+    static getRandomNumber(){
+        return Math.random();
+    }
+}
